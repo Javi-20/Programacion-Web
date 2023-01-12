b90f6192c1d6d1ecfc0f0b05823944c4547be927
Javi <sreivaj18gmail.com>
2023-01-12 17:00:26 -0600

    Semana 6 actividad 6 ejercicios de js FUNCIONES

--- js/script.js
+++ js/script.js
@@ -1,44 +1,102 @@
-let verdaBool = true;
-let indefinido = undefined;
-let num = 31;
-let cadenaDeTexto = "¡¡Feliz Navidad y Prospero Año nuevo!!"
-let cero = null;
+/*ACTIVIDAD 5 Semana 5*/
+/*
+$(document).ready(function(){
 
-const rfc = "RASM0923NLP05";
+	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
-//VARIABLES
-console.warn("VARIABLES");
-console.log("number: " + num);
-console.log("String: " + cadenaDeTexto);
-console.log("boolean: " + verdaBool);
-console.log("undefined: " + indefinido);
-console.log("null: " + cero);
+	// FILTRANDO PRODUCTOS  ============================================
+
+	$('.category_item').click(function(){
+		var catProduct = $(this).attr('category');
+		console.log(catProduct);
+
+		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+		$('.category_item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
+
+		// OCULTANDO PRODUCTOS =========================
+		$('.product-item').css('transform', 'scale(0)');
+		function hideProduct(){
+			$('.product-item').hide();
+		} setTimeout(hideProduct,400);
+
+		// MOSTRANDO PRODUCTOS =========================
+		function showProduct(){
+			$('.product-item[category="'+catProduct+'"]').show();
+			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
+		} setTimeout(showProduct,400);
+	});
+
+	// MOSTRANDO TODOS LOS PRODUCTOS =======================
+
+	$('.category_item[category="all"]').click(function(){
+		function showAll(){
+			$('.product-item').show();
+			$('.product-item').css('transform', 'scale(1)');
+		} setTimeout(showAll,400);
+	});
+});*/
 
 
-//CONSTANTE
-console.warn("CONSTANTE");
-console.log("Su RFC: " + rfc)
 
-//UN SALUDITO
-var saludo = "Hola, como estas";
-var nombre = "Javi :)";
+/*ACTIVIDAD 6 Semana 6*/
 
-//CONCATENAMOS EL SALUDO
-console.warn("CONCATENACION");
-console.log(saludo + " " + nombre)
+//1. Una función que ejecute una suma de 3 números, imprimir su resultado en la terminal con console.log(). 
+console.log("\t----- Función SUMA ----- ");
+function suma(a, b, c) {
+    let resulado = a + b + c;
+    console.log(`El resultado de ${a} + ${b} + ${c} es igual a ${resulado}`);
+}
+suma(30, 60, 90);
 
-console.warn("INTERPOLACION");
-console.log(`${saludo} ${nombre}`);
+//2. Una función que ejecute una comparación de 2 números (mayor, menor, igual o diferentes), imprimir su resultado en la terminal con console.log().
+console.log("\t----- Función COMPARACIÓN ----- ");
+function comparacion(nume1, nume2) {
+    if(nume1 > nume2) {
+        console.log(`${nume1} es mayor que ${nume2}`);
+    } else if (nume2 > nume1) {
+        console.log(`${nume2} es mayor que ${nume1}`);
+    } else {
+        console.log("Los números son iguales");
+    }
+}
+comparacion(289, 289);
 
-//VARIABLES PARA HACER UNA SUMA
-var num1 = 52;
-var num2 = 120;
+//3. Realizar las 2 funciones anteriores, pero como función flecha, imprimir su resultado en la terminal con console.log().
+console.log("\t----- Función FLECHA SUMA ----- ");
+let sumFlech = (a, b, c) => console.log(`El resultado de ${a} + ${b} + ${c} es igual a ${a + b +c}`);
+sumFlech(56, 36, 1000);
 
-console.warn("SUMAR");
-var suma = num1 + num2;
-console.log(`${num} + ${num} = ${suma}`);
+console.log("\t----- Función COMPARACIÓN ----- ");
+let comparacionFlech=(nume1, nume2)=>{
+    if(nume1 > nume2) {
+        console.log(`${nume1} es mayor que ${nume2}`);
+    } else if (nume2 > nume1) {
+        console.log(`${nume2} es mayor que ${nume1}`);
+    } else {
+        console.log("Los números son iguales");
+    }
+}
+comparacionFlech(888, 289);
 
-//HACEMOS UNA COMPARACION
-console.warn("COMPARACION");
-var comparacion = num === num;
-console.log(`${num} === ${num} es ${comparacion}`)
+//4.Crear un objeto con mínimo 5 key(clave) y realizar la desestructuración de 3 de ellas. Imprimir su resultado en la terminal con console.log().
+console.log("\t----- Comprar un celular ----- ");
+comprarCelular ={
+    id:"Tel200501",
+    cliente: "Javi",
+    edad:22,
+    estado: "Puebla",
+    producto:["Samsung J6","Samsung A52","Motorola G3"]
+};
+let {id,cliente,edad,estado,producto} = comprarCelular;
+console.log(" El cliente " + cliente + " con id de la tienda " + id + " tiene " + edad + " años del estado de " + estado + 
+" hizo la compra de 3 celulares, "  + producto)
+
+//5. Crear un arreglo que contenga: 1-string, 2-números, 1-array con 2 valores y realizar la desestructuración de 3 de ellas. Imprimir su resultado en la terminal con console.log().
+console.log("\t----- ARREGLO ----- ");
+let arreglo = ["Hola!!! ¿como estas?", 20, 22, [true, false]];
+const [bienvenida, dia, año] = arreglo;
+console.log(bienvenida);
+console.log("Mi cumpleaños es el día " + dia + " de mayo");
+console.log("Y cumpliré " + año + " años ");
